35dc24979fb86c05ca5fc11a35e099b8a95340c1
laupwing <laupwing@gmail.com>
2025-07-22 20:45:53 +0200

    Add initial implementation of agent module with necessary imports

---
+++ tutorial/agent.py
@@ -0,0 +1,11 @@
+import os
+import asyncio
+from google.adk.agents import Agent
+from google.adk.models.lite_llm import LiteLlm # For multi-model support
+from google.adk.sessions import InMemorySessionService
+from google.adk.runners import Runner
+from google.genai import types # For creating message Content/Parts
+
+import warnings
+# Ignore all warnings
+warnings.filterwarnings("ignore")
