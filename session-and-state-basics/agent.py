a758103d829319cb5e7c8fd417baf89ce6224246
laupwing <laupwing@gmail.com>
2025-07-28 19:31:50 +0200

    Update initial user state with specific preferences and name

--- session-and-state-basics/agent.py
+++ session-and-state-basics/agent.py
@@ -9,8 +9,11 @@ load_dotenv()
 session_service_stateful = InMemorySessionService()
 
 initial_state = {
-    "user_name": "John Doe",
+    "user_name": "Brandon Hancock",
     "user_preferences": """
-    
+        I like to play Pickleball, Disc Golf, and Tennis.
+        My favorite food is Mexican.
+        My favorite TV show is Game of Thrones.
+        Loves it when people like and subscribe to his YouTube channel.
     """
 }
