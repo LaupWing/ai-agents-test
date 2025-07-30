45b6c3b56df0106e029b3e33ede35cbf6598636c
laupwing <laupwing@gmail.com>
2025-07-30 09:50:49 +0200

    Refactor session creation in main.py to streamline InMemorySessionService usage for both GPT and Claude agents

--- tutorail_multiple_llms/main.py
+++ tutorail_multiple_llms/main.py
@@ -1,30 +1,32 @@
 import asyncio
 from agents.weather_gpt import build_gpt_agent
 from agents.weather_claude import build_claude_agent
-from sessions.memory import create_session_service
 from runners.runner_factory import build_runner
 from utils.interaction import call_agent_async
 from config import APP_NAME, USER_ID
 from google.adk.sessions import InMemorySessionService
 
 async def main():
-    # session_gpt = await create_session_service().create_session(
-    #     app_name=APP_NAME, 
-    #     user_id=USER_ID, 
-    #     session_id="gpt_session"
-    # )
-    session_service_claude = InMemorySessionService()
-    await session_service_claude.create_session(
+    session_gpt = InMemorySessionService()
+    await session_gpt.create_session(
+        app_name=APP_NAME,
+        user_id=USER_ID,
+        session_id="gpt_session"
+    )
+    session_claude = InMemorySessionService()
+    await session_claude.create_session(
         app_name=APP_NAME,
         user_id=USER_ID,
         session_id="claude_session"
     )
+    
 
-    # runner_gpt = build_runner(build_gpt_agent(), session_gpt)
-    runner_claude = build_runner(build_claude_agent(), session_service_claude)
 
-    # await call_agent_async("What's the weather in Tokyo?", runner_gpt, USER_ID, "gpt_session")
-    await call_agent_async("What's the weather in London?", runner_claude, USER_ID, "claude_session")
+    runner_gpt = build_runner(build_gpt_agent(), session_gpt)
+    runner_claude = build_runner(build_claude_agent(), session_claude)
+
+    await call_agent_async("What's the weather in Amsterdm?", runner_gpt, USER_ID, "gpt_session")
+    await call_agent_async("What's the weather in Amsterdm?", runner_claude, USER_ID, "claude_session")
 
 if __name__ == "__main__":
     asyncio.run(main())
