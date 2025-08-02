e5de19a0e742116f2c8a56f8d144bbf36e7bbdc6
laupwing <laupwing@gmail.com>
2025-08-02 22:44:56 +0200

    udpated

--- tutorial_with_memory/main.py
+++ tutorial_with_memory/main.py
@@ -8,7 +8,7 @@ from google.adk.sessions import InMemorySessionService
 async def main():
     print("\n--- Testing Agent Team Delegation ---")
     session_service = InMemorySessionService()
-    session  = await session_service.create_session(
+    await session_service.create_session(
         app_name=APP_NAME,
         user_id=USER_ID,
         session_id=SESSION_ID
